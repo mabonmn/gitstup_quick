a42b51f883c4e788b42163ae07fc45bf2732f999
Mabon Manoj Ninan <mabonmn2002@gmail.com>
2024-10-18 12:25:28 -0500

    Update setup-git.sh

--- setup-git.sh
+++ setup-git.sh
@@ -1,17 +1,33 @@
+#!/bin/bash
+
 echo 'RUN THIS SCRIPT WITH YOUR GITHUB USERNAME AND EMAIL AS ARGUMENTS'
+
+# Update the package list and install necessary tools
 apt update 
-apt install ncat
+apt install -y ncat
+
+# Add GitHub to SSH config (without proxy)
 echo Host github.com >> ~/.ssh/config
-echo '        Hostname ssh.github.com' >> ~/.ssh/config
-echo '        Port 443' >> ~/.ssh/config
-echo '        ProxyCommand ncat --proxy-type http --proxy bmiproxyp.chmcres.cchmc.org:80 %h %p' >> ~/.ssh/config 
-gituser=$1 
+echo '        Hostname github.com' >> ~/.ssh/config
+echo '        User git' >> ~/.ssh/config
+
+# Set Git username and email
+gituser=$1
 gitemail=$2
-echo $gitemail
-git config --global user.name $gituser
-git config --global user.email $gitemail
-ssh-keygen '-t' ed25519 '-C' $gitemail
+echo "Setting Git username: $gituser"
+echo "Setting Git email: $gitemail"
+
+git config --global user.name "$gituser"
+git config --global user.email "$gitemail"
+
+# Generate SSH key for GitHub access
+ssh-keygen -t ed25519 -C "$gitemail"
+
+# Display the public key for copying to GitHub
 cat ~/.ssh/id_ed25519.pub
-chown root ~/.ssh/config
+
+# Set permissions for SSH config file
 chmod 600 ~/.ssh/config
+
+# Inform the user to copy the SSH key to GitHub
 echo "Copy the above key to GITHUB and try ssh -T git@github.com"
